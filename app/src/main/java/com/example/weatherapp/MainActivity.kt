bd32d2eb465858f1f7e99eace969d982c8c09de2
balram gupta <balramg5643@gmail.com>
2024-09-19 21:31:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val weatherViewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
+
+        setContent {
+            WeatherAppTheme {
+                Surface(modifier = Modifier.fillMaxSize()
+                    .padding(top=8.dp),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WeatherPage(weatherViewModel)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/api/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp.api
+
+object Constants {
+    val apiKey = "ecb0b81f9fbc4caa87e90401241909"
+}
